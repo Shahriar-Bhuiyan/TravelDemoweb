fbecc6d0cf03b10912bb75de595bd690d2e26990
shahriar-bhuiyan <shahriarbhuiyan1@gmail.com>
2021-12-27 15:55:15 +0600

    third cimmit

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,4 +37,23 @@ function scrollHeader(){
     console.log(this.scrollY)
     
 }
-window.addEventListener("scroll",scrollHeader); 
+window.addEventListener("scroll",scrollHeader);
+
+// Swiper Discover
+
+var swiper = new Swiper(".discover_container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
